10e9bb533596a5ab75b8a327c53e35d3cb39bc45
Guillem Vidal <g.vidal.cantavella@gmail.com>
2015-04-27 01:23:14 +0200

    cache matrix methods added

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,22 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# It basically receives the matrix so you can access to it at any time with get()
+# You can override the current matrix with set().
+# You get the result of its inverse matrix, however, the function to calculate it, it'll be calculated just once :)
 
+makeCacheMatrix <- function(x = matrix()) {
+  inverse <<- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse_matrix) inverse <<- inverse_matrix
+  getinverse <- function() inverse
+  list(set = set, get = get,
+       getinverse = getinverse,
+       setinverse)
 }
 
 
@@ -12,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
